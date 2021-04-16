8cf633b0d15786291130123c438bc6dc4d43227b
Gabriel Simmer <gabriel@circleci.com>
2021-04-16 13:47:19 +0100

    Actually working certificate importing and sln building.

--- src/scripts/pack-exe.ps1
+++ src/scripts/pack-exe.ps1
@@ -4,12 +4,24 @@ if ( $Env:PACK_MAPPING_FILE -ne "") {
     "/f ${Env.PACK_MAPPING_FILE}"
 }
 
-$cert = $ExecutionContext.InvokeCommand.ExpandString($Env:PACK_SIGNING_CERT)
-$cert_pass = $ExecutionContext.InvokeCommand.ExpandString($Env:PACK_CERT_PASSWORD) | ConvertTo-SecureString
+$certificate = $ExecutionContext.InvokeCommand.ExpandString($Env:PACK_SIGNING_CERT)
+$cert_pass = $ExecutionContext.InvokeCommand.ExpandString($Env:PACK_CERT_PASSWORD)
 
-[System.Text.Encoding]::ASCII.GetString([System.Convert]::FromBase64String($cert)) | Out-File -Encoding "ASCII" cert.pfx
-Import-PfxCertificate -FilePath ./cert.pfx -CertStoreLocation Cert:\CurrentUser\Root -Password $cert_pass
+[System.Convert]::FromBase64String($certificate) | Set-Content cert.pfx -Encoding Byte 
+# From https://gist.github.com/jrahme-cci/30a135c50db93a27bba649ddfb054661
+$Cert = Import-PfxCertificate -FilePath ./cert.pfx -Password (ConvertTo-SecureString -String "$cert_pass" -AsPlainText -Force) -CertStoreLocation Cert:\LocalMachine\My
+Export-Certificate -Cert $Cert -File c:\Cert.sst -Type SST
+Import-Certificate -File c:\cert.sst  -CertStoreLocation Cert:\LocalMachine\Root 
+Import-Certificate -File c:\cert.sst -CertStoreLocation Cert:\CurrentUser\My
+
 
 $makeappx = "C:\Program Files (x86)\Windows Kits\10\bin\${Env.PACK_WINDOWS_SDK}\x64\makeappx.exe"
 
 & $makeappx pack /d $Env:PACK_INPUT_DIR /p $Env:PACK_PACKAGE_NAME $parameters
+
+$compress = @{
+  Path = "AppPackages"
+  CompressionLevel = "Fastest"
+  DestinationPath = "AppPackages.zip"
+}
+Compress-Archive @compress
--- src/scripts/pack-sln.ps1
+++ src/scripts/pack-sln.ps1
@@ -2,10 +2,21 @@ $parameters = ""
 
 $msbuild = "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe"
 
-$cert = $ExecutionContext.InvokeCommand.ExpandString($Env:PACK_SIGNING_CERT)
-$cert_pass = $ExecutionContext.InvokeCommand.ExpandString($Env:PACK_CERT_PASSWORD) | ConvertTo-SecureString
+$certificate = $ExecutionContext.InvokeCommand.ExpandString($Env:PACK_SIGNING_CERT)
+$cert_pass = $ExecutionContext.InvokeCommand.ExpandString($Env:PACK_CERT_PASSWORD)
 
-[System.Text.Encoding]::ASCII.GetString([System.Convert]::FromBase64String($cert)) | Out-File -Encoding "ASCII" cert.pfx
-Import-PfxCertificate -FilePath ./cert.pfx -CertStoreLocation Cert:\CurrentUser\Root -Password $cert_pass
+[System.Convert]::FromBase64String($certificate) | Set-Content cert.pfx -Encoding Byte 
+# From https://gist.github.com/jrahme-cci/30a135c50db93a27bba649ddfb054661
+$Cert = Import-PfxCertificate -FilePath ./cert.pfx -Password (ConvertTo-SecureString -String "$cert_pass" -AsPlainText -Force) -CertStoreLocation Cert:\LocalMachine\My
+Export-Certificate -Cert $Cert -File c:\Cert.sst -Type SST
+Import-Certificate -File c:\cert.sst  -CertStoreLocation Cert:\LocalMachine\Root 
+Import-Certificate -File c:\cert.sst -CertStoreLocation Cert:\CurrentUser\My
 
 & $msbuild /p:Platform=x64
+
+$compress = @{
+  Path = "AppPackages"
+  CompressionLevel = "Fastest"
+  DestinationPath = "AppPackages.zip"
+}
+Compress-Archive @compress
