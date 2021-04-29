15e381a3caa73bcfa71a5fd6377387fa3b75208c
Gabriel Simmer <gabriel@circleci.com>
2021-04-29 16:18:28 +0100

    Remove bytestream encoding.

--- src/scripts/pack-sln.ps1
+++ src/scripts/pack-sln.ps1
@@ -13,7 +13,7 @@ if ($Env:PACK_IMPORT_CERT -eq 1) {
   $certificate = $ExecutionContext.InvokeCommand.ExpandString($Env:PACK_SIGNING_CERT)
   $cert_pass = $ExecutionContext.InvokeCommand.ExpandString($Env:PACK_CERT_PASSWORD)
 
-  [System.Convert]::FromBase64String($certificate) | Set-Content cert.pfx -AsByteStream
+  [System.Convert]::FromBase64String($certificate) | Set-Content cert.pfx
   # From https://gist.github.com/jrahme-cci/30a135c50db93a27bba649ddfb054661
   $Cert = Import-PfxCertificate -FilePath ./cert.pfx -Password (ConvertTo-SecureString -String "$cert_pass" -AsPlainText -Force) -CertStoreLocation Cert:\LocalMachine\My
   Export-Certificate -Cert $Cert -File Temp:\cert.sst -Type SST
