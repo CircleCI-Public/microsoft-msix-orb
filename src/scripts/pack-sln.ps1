e685f3384d6c186a776065fd4c769a456d2f43c3
Gabriel Simmer <gabriel@circleci.com>
2021-04-29 17:17:01 +0100

    Testing parameter positioning.

--- src/scripts/pack-sln.ps1
+++ src/scripts/pack-sln.ps1
@@ -7,7 +7,7 @@ $parameters = if ($Env:PACK_PARAMETERS -eq $null) {
   $Env:PACK_PARAMETERS
 }
 
-$msbuild = "${Env:ProgramFiles(x86)}\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe"
+$msbuild = "${Env:ProgramFiles(x86)}\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe $parameters"
 
 if ($Env:PACK_IMPORT_CERT -eq 1) {
   $certificate = $ExecutionContext.InvokeCommand.ExpandString($Env:PACK_SIGNING_CERT)
@@ -16,7 +16,7 @@ if ($Env:PACK_IMPORT_CERT -eq 1) {
   [System.Convert]::FromBase64String($certificate) | Set-Content C:\cert.pfx -AsByteStream
   Import-PfxCertificate -FilePath C:\cert.pfx -Password (ConvertTo-SecureString -String "$cert_pass" -AsPlainText -Force) -CertStoreLocation Cert:\CurrentUser\My
 }
-& $msbuild $parameters
+& $msbuild
 
 $compress = @{
   Path = "AppPackages"
