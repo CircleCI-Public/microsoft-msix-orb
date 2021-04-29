f535240c5d84be941c36b5a84e5b43e956cbc0fc
Gabriel Simmer <gabriel@circleci.com>
2021-04-29 10:24:56 +0100

    Revert temp drive.

--- src/scripts/sign.ps1
+++ src/scripts/sign.ps1
@@ -17,12 +17,12 @@ if ($Env:SIGN_IMPORT_CERT -eq 1) {
   $certificate = $ExecutionContext.InvokeCommand.ExpandString($Env:SIGN_SIGNING_CERT)
   $cert_pass = $ExecutionContext.InvokeCommand.ExpandString($Env:SIGN_CERT_PASSWORD)
 
-  [System.Convert]::FromBase64String($certificate) | Set-Content temp:/cert.pfx -AsByteStream
+  [System.Convert]::FromBase64String($certificate) | Set-Content C:\cert.pfx -AsByteStream
   # From https://gist.github.com/jrahme-cci/30a135c50db93a27bba649ddfb054661
-  $Cert = Import-PfxCertificate -FilePath temp:/cert.pfx -Password (ConvertTo-SecureString -String "$cert_pass" -AsPlainText -Force) -CertStoreLocation Cert:\LocalMachine\My
-  Export-Certificate -Cert $Cert -File temp:\cert.sst -Type SST
-  Import-Certificate -File temp:\cert.sst  -CertStoreLocation Cert:\LocalMachine\Root 
-  Import-Certificate -File tmp:\cert.sst -CertStoreLocation Cert:\CurrentUser\My
+  $Cert = Import-PfxCertificate -FilePath C:\cert.pfx -Password (ConvertTo-SecureString -String "$cert_pass" -AsPlainText -Force) -CertStoreLocation Cert:\LocalMachine\My
+  Export-Certificate -Cert $Cert -File C:\cert.sst -Type SST
+  Import-Certificate -File C:\cert.sst  -CertStoreLocation Cert:\LocalMachine\Root 
+  Import-Certificate -File C:\cert.sst -CertStoreLocation Cert:\CurrentUser\My
 }
 
 $signtool = "${Env:ProgramFiles(x86)}\Windows Kits\10\bin\${Env:SIGN_WINDOWS_SDK}\x64\signtool.exe"
