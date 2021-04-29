946b4ade1ed1cff29d1cb4f3ad1df4171b79a9d4
Gabriel Simmer <gabriel@circleci.com>
2021-04-29 17:26:49 +0100

    Revert to IEX.

--- src/scripts/pack-sln.ps1
+++ src/scripts/pack-sln.ps1
@@ -1,13 +1,13 @@
 $ErrorActionPreference = "Stop" 
 
-$default_parameters = @("/p:Platform=x64")
+$default_parameters = "/p:Platform=x64"
 $parameters = if ($Env:PACK_PARAMETERS -eq $null) {
   $default_parameters
 } else {
   $Env:PACK_PARAMETERS
 }
 
-$msbuild = "${Env:ProgramFiles(x86)}\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe $parameters"
+$msbuild = "${Env:ProgramFiles(x86)}\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe"
 
 if ($Env:PACK_IMPORT_CERT -eq 1) {
   $certificate = $ExecutionContext.InvokeCommand.ExpandString($Env:PACK_SIGNING_CERT)
@@ -16,7 +16,7 @@ if ($Env:PACK_IMPORT_CERT -eq 1) {
   [System.Convert]::FromBase64String($certificate) | Set-Content C:\cert.pfx -AsByteStream
   Import-PfxCertificate -FilePath C:\cert.pfx -Password (ConvertTo-SecureString -String "$cert_pass" -AsPlainText -Force) -CertStoreLocation Cert:\CurrentUser\My
 }
-& $msbuild
+"& $msbuild $parameters" | iex
 
 $compress = @{
   Path = "AppPackages"
