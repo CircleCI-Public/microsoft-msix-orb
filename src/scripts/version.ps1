ee90dae6992716aed1f5ae0dbf5c23303354dcc8
Gabriel Simmer <gabriel@circleci.com>
2021-04-30 09:50:00 +0100

    Remove version command, fix up config and orb description.

--- src/scripts/version.ps1
+++
@@ -1,4 +0,0 @@
-# Update appxmanifest. This must be done before the build.
-[xml]$manifest = get-content ".\Msix\Package.appxmanifest"
-$manifest.Package.Identity.Version = "$(major).$(minor).$(build).$(revision)"    
-$manifest.save("Msix/Package.appxmanifest")
